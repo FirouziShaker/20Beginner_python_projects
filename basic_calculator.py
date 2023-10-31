2643f6ee38253c48b34114b3f53800a2b84a6358
FirozuiShaker <furouzi.abolfazl@yahoo.com>
2023-10-31 16:44:03 +0100

    basic calc. codes

---
+++ basic_calculator.py
@@ -0,0 +1,40 @@
+# define the functions needed: add, sub, mul, div
+# print options to the user
+# ask for values
+# call the functions
+# while loop to continue the program until the user wants to exit
+
+
+
+def add(a,b):
+    answer= a + b
+    print (str(a) + " + " + str(b) + " = " + str(answer))
+
+def sub(a,b):
+    answer= a - b
+    print (str(a) + " - " + str(b) + " = " + str(answer))
+
+def mul(a,b):
+    answer= a * b
+    print (str(a) + " * " + str(b) + " = " + str(answer))
+
+def div(a,b):
+    answer= a / b
+    print (str(a) + " / " + str(b) + " = " + str(answer))
+    
+
+a= int(input ("Please enter your first number :"))
+options= input (" Please select + , / , * or  -  : ")
+b= int(input ("Please enter your second number :"))
+
+if options=="+":
+    add(a,b)
+elif options=="-":
+    add(a,b)
+elif options=="*":
+    mul(a,b)
+elif options=="/":
+    div(a,b)
+else:
+    print("Please enter correct option  ( + , * , - , / )")
+    
--- word_replacement.py
+++ word_replacement.py
@@ -1,14 +1,21 @@
 def replace_word():
     
     print("*"*20)
-    str="Hello all , we are here to know Germany as well as possible"
+    
+    str="Hello all , we are here to know Germany as well as possible".lower()
+   
     print(str)
+    
     print("*"*20)
-    select_word_to_replace=input("Please choose a word to replace: ")
-    replacement_word=input("Please say, what do you want to replace instead that: ")
+    
+    select_word_to_replace=input("Please choose a word to replace: ").lower()
+    replacement_word=input("Please say, what do you want to replace instead that: ").lower()
     new_str=str.replace(select_word_to_replace,replacement_word)
+    
     print("*"*20)
+    
     print(new_str)
+    
     print("*"*20)
 
 replace_word()
